7d859b5a5e375c08c81ebfa14931111e741df371
poilynx <xilyyn@example.com>
2018-02-18 09:45:07 +0800

    :ok_hand:

--- main.c
+++ main.c
@@ -2,25 +2,29 @@
 #include <windows.h>
 #include "resource.h"
 INT_PTR CALLBACK DialogProc(
-  _In_ HWND   hwndDlg,
+  _In_ HWND   hDlg,
   _In_ UINT   uMsg,
   _In_ WPARAM wParam,
   _In_ LPARAM lParam
 ) {
 	switch(uMsg) {
+	case WM_INITDIALOG:
+		return TRUE;
 	case WM_COMMAND:
+		
 		switch(wParam) {
 		case IDOK:
 			printf("ok\n");
 			break;
 		case IDCANCEL:
 			printf("cancel\n");
+			DestroyWindow(hDlg);
 			PostQuitMessage(0);
 			break;
 		}
 		break;
 	}
-	return 0;
+	return FALSE;
 }
 int main() {
 	CHAR szBuf[1024];
@@ -30,13 +34,13 @@ int main() {
 	LoadStringA(NULL,IDS_1,szBuf,sizeof(szBuf));
 	printf("IDS_1  =  %s\n", szBuf);
 
-	hDlgModeless = CreateDialog(NULL,MAKEINTRESOURCE(IDD_ABOUT_DIALOG),NULL,DialogProc);
+	hDlgModeless = CreateDialog(NULL,MAKEINTRESOURCE(IDD_DIALOG1),NULL,DialogProc);
 	if(hDlgModeless == 0) {
 		printf("CreateDialog error %d\n",hDlgModeless);
 		return -1;
 	}
 
-	ShowWindow(hDlgModeless,SW_SHOW);
+	//ShowWindow(hDlgModeless,SW_SHOW);
 
 	while (GetMessage (&msg, NULL, 0, 0))
 	{
--- resource.h
+++ resource.h
@@ -2,5 +2,6 @@
 #define RESOURCE_H
 #define IDS_1 1
 #define IDS_2 2
-#define IDD_ABOUT_DIALOG 3
+#define IDD_DIALOG1 3
+#define IDI_ICON 127
 #endif
